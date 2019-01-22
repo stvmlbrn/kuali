9c8834c9719569308211d01f82b1920c90958ed4
Steve Milburn <scmilburn@gmail.com>
2019-01-22 18:49:46 -0500

    created building.js

---
+++ building.js
@@ -0,0 +1,23 @@
+const Elevator = require('./elevator');
+
+/**
+ *
+ * @param {object} - an object to initialize the building. Keys
+ * should include totalElevators and totalFloors. Defaults to 1 elevator
+ * and 2 floors, so new buildings can be created without passing in
+ * an init object.
+ */
+function Building({ totalElevators = 1, totalFloors = 2 } = {}) {
+  this.floors = Math.max(totalFloors, 2); //must have at least 2 floors to require an elevator
+  this.elevators = [];
+
+  this._init = function() {
+    for (let i = 1; i <= totalElevators; i++) {
+      this.elevators.push(new Elevator(i));
+    }
+  };
+
+  this._init();
+}
+
+module.exports = Building;
