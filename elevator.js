43a45e4dc76e47c9c9c85bc6f8f4caa091a27fa9
Steve Milburn <scmilburn@gmail.com>
2019-01-22 18:20:17 -0500

    move logic in move() and create openDoor()

--- elevator.js
+++ elevator.js
@@ -7,19 +7,50 @@ function Elevator(id) {
   this.direction = '';
   this.trips = 0;
   this.requireMaintenance = false;
+  this.doorIsOpen: false;
 }
 
 Elevator.prototype.move = function(targetFloor) {
+
   this.moving = true;
+  /**
+   * Increment or decrement the instance floor value before
+   * while loop to avoid reporting in the event of only moving
+   * 1 floor.
+   */
   if (targetFloor > this.floor) {
     this.direction = 'up';
     this.floor++;
 
     while (this.floor < targetFloor) {
-      report.move(this.floor);
+      report.move(this.id, this.floor);
       this.floor++;
+    }
+  } else {
+    this.direction = 'down';
+    this.floor--;
+
+    while (this.floor > targetFloor) {
+      report.move(this.id, this.floor);
+      this.floor--;
     }
   }
 
-  report.arrive(targetFloor);
+  this.moving = false;
+  this.trips++;
+  report.arrive(this.id, targetFloor);
+  this.openDoor();
 };
+
+/**
+ * Set the doorIsOpen status to true and report
+ * that the doors are opening.
+ */
+Elevator.prototype.openDoor = function() {
+  if (this.doorIsOpen) return;
+
+  this.doorIsOpen = true;
+  report.openDoor(this.id);
+}
+
+
